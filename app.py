e0a3e5ea0bf15ee9d3b145538c3260542f68ce92
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-11-02 16:11:23 +0900

    불필요한 모듈 제거

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import json
 
 from flask import Flask, request, make_response
-from flask_restx import Api, Resource, reqparse
+from flask_restx import Api, Resource
 from model import Predict
-import werkzeug
+
 
 app = Flask(__name__)
 api = Api(app,
@@ -20,7 +20,7 @@ class Test(Resource):
     def post(self):  # put application's code here
 
         input_img = request.files['image']
-        img = input_img.read()# image 는 키값 value는 파일
+        img = input_img.read()  # image 는 키값 value는 파일
         model = Predict()
         res = str(model.predict(img=img))  # split 함수 사용을 위해 tuple 을 str 으로 변경
         cropName, sickNameKor = res.split('_')  # 문자열 나누기
