3d6f419170aad535c0b436a893aeb166f061eb8d
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-12-09 11:25:15 +0900

    Feat : json 응답 신뢰도 추가
    모델의 신뢰도 리턴

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import json
 
 from flask import Flask, request, make_response
-from flask_restx import Api, Resource, reqparse
+from flask_restx import Api, Resource
 from model import Predict
-import werkzeug
 
 app = Flask(__name__)
 api = Api(app,
@@ -12,7 +11,9 @@ api = Api(app,
           description="Mobilenet",
           terms_url="/predict",
           contact="wlgud3412@naver.com")
+
 app.config['UPLOAD_FOLDER'] = 'uploaded\\image'
+model = Predict()
 
 
 @api.route('/predict')
@@ -20,16 +21,21 @@ class Test(Resource):
     def post(self):  # put application's code here
 
         input_img = request.files['image']
-        img = input_img.read()# image 는 키값 value는 파일
-        model = Predict()
-        res = str(model.predict(img=img))  # split 함수 사용을 위해 tuple 을 str 으로 변경
-        cropName, sickNameKor = res.split('_')  # 문자열 나누기
+        img = input_img.read()  # image 는 키값 value는 파일
+        res = model.predict(img=img)
+        label = str(res[0])  # split 함수 사용을 위해 tuple 을 str 으로 변경
+        cropName, sickNameKor = label.split('_')  # 문자열 나누기
+        confidence = '{:2.0f}'.format(res[1])
+        confidence = int(confidence)
         json_data = {
+
             'cropName': cropName,
-            'sickNameKor': sickNameKor
+            'sickNameKor': sickNameKor,
+            'confidence': confidence
         }
         return make_response(json.dumps(json_data, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    model.model_load()
+    app.run(host='0.0.0.0',port=80)
